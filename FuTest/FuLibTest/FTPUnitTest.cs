5bc004aa9cfc8a30e8859dd0fd79374967a3a8ac
phiree <phiree@gmail.com>
2012-12-25 22:50:23 +0800

    服务器端创建文件夹/ 使用 文件监视的方式触发上传时间

---
+++ FuTest/FuLibTest/FTPUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FuLib;
+namespace FuTest.FuLibTest
+{
+    public class FTPUnitTest
+    {
+        [Fact]
+        public void FtpDirectoryExistsTest()
+        {
+            string errMsg;
+        
+         bool resut= FtpUnit.EnsureFtpPath("ftp://127.0.0.1/sdfdsafdasf/dd/", "", "",out errMsg);
+         Assert.True(resut);
+            
+        }
+    }
+}
