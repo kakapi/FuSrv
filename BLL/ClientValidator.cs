b66a9c75859bac43ebee843b2013f5d2fcfb8d19
phiree <phiree@gmail.com>
2013-01-02 19:34:20 +0800

    ADD SERVER VALIDATION

---
+++ BLL/ClientValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace BLL
+{
+    //客户端数量验证
+    public class ClientCounter
+    {
+        private static bool ValidClients(out string clientstring)
+        {
+            bool result = false;
+         
+            clientstring = string.Empty;
+
+            string encryptedString = string.Empty;
+            try
+            {
+                string encryptedFilePath = "";
+                 encryptedString = File.ReadAllText(encryptedFilePath);
+               clientstring=   FuLib.Crypto.DecryptStringAES(encryptedString, "P@ssw0rd");
+            }
+            catch
+            {
+                result = false;
+                
+            }
+
+          
+
+            return result;
+        }
+        public static void GetClients()
+        {
+            
+
+        }
+    }
+    
+}
---
+++ FuLib/Logger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using log4net;
+using log4net.Repository.Hierarchy;
+using log4net.Appender;
+using log4net.Layout;
+using FuLib;
+namespace FuLib
+{
+    public class Logger
+    {
+        ILog log = null;
+        public  ILog GetLoggerInstance(string logFilePath)
+        {
+            
+                if (log == null)
+                {
+                    Config(logFilePath);
+                    log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+                }
+                return log;
+           
+        }
+        public static void Config(string logFileName)
+        {
+            Hierarchy hierarchy = (Hierarchy)LogManager.GetRepository();
+            hierarchy.Root.RemoveAllAppenders(); /*Remove any other appenders*/
+            RollingFileAppender fileAppender = new RollingFileAppender();
+            fileAppender.AppendToFile = true;
+            fileAppender.LockingModel = new FileAppender.MinimalLock();
+            fileAppender.RollingStyle = RollingFileAppender.RollingMode.Date;
+            fileAppender.File = logFileName;
+            PatternLayout pl = new PatternLayout();
+            pl.ConversionPattern = "%d [%2%t] %-5p [%-10c] %m%n%n";
+            pl.ActivateOptions();
+            fileAppender.Layout = pl;
+            fileAppender.ActivateOptions();
+            log4net.Config.BasicConfigurator.Configure(fileAppender);
+
+        }
+    }
+}
