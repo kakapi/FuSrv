5761c2fa6608f0ef722b3cd6d19b7473061554ee
phiree <phiree@gmail.com>
2013-01-04 18:13:54 +0800

    服务器tcplisten

--- FUServer/FrmMain.Designer.cs
+++ FUServer/FrmMain.Designer.cs
@@ -28,20 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlAction = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tbxLog = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlAction.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlAction
+            // 
+            this.pnlAction.Controls.Add(this.label1);
+            this.pnlAction.Controls.Add(this.btnStop);
+            this.pnlAction.Controls.Add(this.btnStart);
+            this.pnlAction.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlAction.Location = new System.Drawing.Point(0, 0);
+            this.pnlAction.Name = "pnlAction";
+            this.pnlAction.Size = new System.Drawing.Size(475, 44);
+            this.pnlAction.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(290, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "启动";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Visible = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(371, 13);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Visible = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // tbxLog
+            // 
+            this.tbxLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbxLog.Location = new System.Drawing.Point(0, 44);
+            this.tbxLog.Name = "tbxLog";
+            this.tbxLog.Size = new System.Drawing.Size(475, 218);
+            this.tbxLog.TabIndex = 1;
+            this.tbxLog.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "服务已启动";
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(475, 262);
+            this.Controls.Add(this.tbxLog);
+            this.Controls.Add(this.pnlAction);
             this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmMain_FormClosed);
+            this.pnlAction.ResumeLayout(false);
+            this.pnlAction.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlAction;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox tbxLog;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- FUServer/FrmMain.cs
+++ FUServer/FrmMain.cs
@@ -7,16 +7,135 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.Configuration;
-
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
 namespace FUServer
 {
     public partial class FrmMain : Form
     {
+        static TcpListener listener;
+        const int LIMIT = 5; //5 concurrent clients
+        bool started = true;
         public FrmMain()
         {
             InitializeComponent();
+
+            StartService();
+        }
+        private void SetButtonStatus()
+        {
+            btnStart.Enabled = !started;
+            btnStop.Enabled = started;
+            if (started)
+            {
+                btnStart.Text = "已启动";
+                btnStop.Text = "停止";
+            }
+            else
+            {
+                btnStart.Text = "启动";
+                btnStop.Text = "已停止";
+            }
+        }
+        private  void Log(string message)
+        {
+            new FuLib.Logger().GetLoggerInstance().Info(message);
+            tbxLog.AppendText(DateTime.Now +"  "+ message+Environment.NewLine);
+        }
+        
+        private void StartService()
+        {
+            if (listener == null)
+            {
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("192.168.1.141");
+                listener = new TcpListener(localAddr, port);
+            }
+            try
+            {
+                listener.Start();
+                started = true;
+                SetButtonStatus();
+                Log("服务启动");
+
+                for (int i = 0; i < LIMIT; i++)
+                {
+                    Thread t = new Thread(new ThreadStart(Service));
+                    t.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log("启动出错:"+ex.Message);
+            }
             
         }
+        private  void Service()
+        {
+            try
+            {
+                while (true && started)
+                {
+
+                    Socket soc = listener.AcceptSocket();
+                    //soc.SetSocketOption(SocketOptionLevel.Socket,
+                    //        SocketOptionName.ReceiveTimeout,10000);
+
+                    Log( string.Format("Connected: {0}",
+                                             soc.RemoteEndPoint));
+
+                    try
+                    {
+                        Stream s = new NetworkStream(soc);
+
+                        StreamWriter sw = new StreamWriter(s);
+                        sw.AutoFlush = true; // enable automatic flushing
+
+
+                        sw.WriteLine(DateTime.Now);
+                        sw.WriteLine("");
+
+                    }
+                    catch (Exception e)
+                    {
+
+                    Log(e.Message);
+
+                    }
+
+               Log( string.Format("Disconnected: {0}", 
+                                        soc.RemoteEndPoint));
+
+                    soc.Close();
+                }
+            }
+            catch { }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            started = false;
+            listener.Stop();
+            
+            SetButtonStatus();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartService();
+           
+        }
+
+        private void FrmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            started = false;
+            listener.Stop();
+            Application.ExitThread();
+            
+        }
+
         
     }
 }
